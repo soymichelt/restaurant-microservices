35800f8f6069eb75148723faa8f16725a1699a95
SoymichelDev <mtraatabladaa94@gmail.com>
2024-01-20 00:44:44 -0600

    refactor: remove domain services unused

--- kitchen/src/shared/domain/services/encriptionService.ts
+++
@@ -1,4 +0,0 @@
-export interface EncriptionService {
-  encrypt(textToCipher: string): Promise<string>;
-  decrypt(ciphertext: string): Promise<string>;
-}
--- kitchen/src/shared/domain/services/mailingService.ts
+++
@@ -1,9 +0,0 @@
-export type SendEmailProps = {
-  to: string;
-  subject: string;
-  message: string;
-};
-
-export interface MailingService {
-  send(props: SendEmailProps): Promise<void>;
-}

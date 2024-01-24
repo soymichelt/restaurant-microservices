f63dfb0b854ad1550ba98fc4cee8533dfd5204f0
SoymichelDev <mtraatabladaa94@gmail.com>
2024-01-24 02:50:19 -0600

    feat: add signup use case

--- account/src/di/services/users.ts
+++ account/src/di/services/users.ts
@@ -1,21 +1,21 @@
 import { container } from '@di/shared';
 import { GetAllUsersUseCase } from '@services/users/application/useCases/all/getAllUsersUseCase';
-import { CreateUserUseCase } from '@services/users/application/useCases/create/createUserUseCase';
 import { NotifyUserRegisteredUseCase } from '@services/users/application/useCases/notifRegistered/notifyUserRegistedUseCase';
 import { NotifyUserRemovedUseCase } from '@services/users/application/useCases/notifRemoved/notifyUserRemovedUseCase';
 import { RemoveUserUseCase } from '@services/users/application/useCases/remove/removeUserUseCase';
+import { SigninUseCase } from '@services/users/application/useCases/signin/signinUseCase';
+import { SingupUserUseCase } from '@services/users/application/useCases/signup/singupUserUseCase';
 import { UserRepository } from '@services/users/domain/repositories/userRepository';
-import { CreateUserController } from '@services/users/infrastructure/functions/http/create/controller';
 import { GetAllUsersController } from '@services/users/infrastructure/functions/http/getAll/controller';
 import { RemoveUserController } from '@services/users/infrastructure/functions/http/remove/controller';
+import { SigninController } from '@services/users/infrastructure/functions/http/signin/controller';
+import { SignupUserController } from '@services/users/infrastructure/functions/http/signup/controller';
 import { NotifyUserRegisteredController } from '@services/users/infrastructure/functions/sns/notifRegistered/controller';
 import { NotifyUserRemovedController } from '@services/users/infrastructure/functions/sns/notifRemoved/controller';
 import { MongoUserRepository } from '@services/users/infrastructure/persistence/mongodb/MongoUserRepository';
 
 container
   .register<UserRepository>('UserRepository', MongoUserRepository)
-  .register<CreateUserController>('CreateUserController', CreateUserController)
-  .register<CreateUserUseCase>('CreateUserUseCase', CreateUserUseCase)
   .register<GetAllUsersUseCase>('GetAllUsersUseCase', GetAllUsersUseCase)
   .register<GetAllUsersController>('GetAllUsersController', GetAllUsersController)
   .register<NotifyUserRegisteredUseCase>('NotifyUserRegisteredUseCase', NotifyUserRegisteredUseCase)
@@ -23,6 +23,10 @@ container
   .register<RemoveUserUseCase>('RemoveUserUseCase', RemoveUserUseCase)
   .register<RemoveUserController>('RemoveUserController', RemoveUserController)
   .register<NotifyUserRemovedUseCase>('NotifyUserRemovedUseCase', NotifyUserRemovedUseCase)
-  .register<NotifyUserRemovedController>('NotifyUserRemovedController', NotifyUserRemovedController);
+  .register<NotifyUserRemovedController>('NotifyUserRemovedController', NotifyUserRemovedController)
+  .register<SigninUseCase>('SigninUseCase', SigninUseCase)
+  .register<SigninController>('SigninController', SigninController)
+  .register<SingupUserUseCase>('SingupUserUseCase', SingupUserUseCase)
+  .register<SignupUserController>('SignupUserController', SignupUserController);
 
 export { container };
--- account/src/services/users/infrastructure/functions/http/signup/handler.ts
+++ account/src/services/users/infrastructure/functions/http/signup/handler.ts
@@ -1,10 +1,10 @@
 import { container } from '@di/services/users';
 import middy from '@middy/core';
-import { CreateUserController } from '@services/users/infrastructure/functions/http/create/controller';
+import { SignupUserController } from '@services/users/infrastructure/functions/http/signup/controller';
 import { APIGatewayProxyEventV2, Context } from 'aws-lambda';
 
 const invokeController = async function (event: APIGatewayProxyEventV2, context: Context) {
-  const controller = container.resolve<CreateUserController>('CreateUserController');
+  const controller = container.resolve<SignupUserController>('SignupUserController');
   return await controller.execute(event, context);
 };
 

fe24b4f0977ea0f14abcd7af46142d556b3618b3
SoymichelDev <mtraatabladaa94@gmail.com>
2024-01-28 21:48:32 -0600

    feat: add userId value object

--- account/src/services/users/domain/user.spec.ts
+++ account/src/services/users/domain/user.spec.ts
@@ -1,13 +1,13 @@
 import { User } from '@services/users/domain/user';
 import { UserEmail } from '@services/users/domain/valueObjects/userEmail';
+import { UserId } from '@services/users/domain/valueObjects/userId';
 import { UserName } from '@services/users/domain/valueObjects/userName';
 import { UserPassword } from '@services/users/domain/valueObjects/userPassword';
-import { Id } from '@shared/domain/valueObjects/id';
 
 describe('Tests User domain entity', () => {
   test('Test create instance object', () => {
     const user = User.create({
-      userId: Id.newId(),
+      userId: UserId.newId(),
       username: UserName.build('Michel'),
       email: UserEmail.build('mtraatabladaa94@gmail.com'),
       password: UserPassword.build('michel@2023'),
